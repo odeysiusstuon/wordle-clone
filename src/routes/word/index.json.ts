416e1dbf243dd6b95ae220a8eddefaedb5b89cfc
therealvidem <superninario@gmail.com>
2022-05-16 02:02:57 -0700

    Rolled vercel adapter back to 1.0.0-next.50 to resolve issues

--- src/routes/word/index.json.ts
+++ src/routes/word/index.json.ts
@@ -1,15 +1,6 @@
 import { db } from '$lib/db';
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export async function get() {
-	console.log(db);
-	console.log(process.env.MONGODB_URI);
-	const client = new MongoClient(process.env.MONGODB_URI);
-	const clientPromise = client.connect();
-	console.log(clientPromise);
 	const word = await db.getLatestPlayerWord();
 	if (word) {
 		return {

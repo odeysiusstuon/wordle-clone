722d57607ad771a4b6b0884a30ec2d6bd865d6ea
therealvidem <superninario@gmail.com>
2022-05-16 01:29:41 -0700

    Readded db debug conditions, console logs for debuggin

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,9 +4,29 @@ import { MongoDB } from './mongodb';
 import { env } from '$lib/env';
 
 const uri = env.MONGODB_URI;
-
+console.log(uri);
 if (!uri) {
 	throw new Error('Could not find URI');
 }
 
-export const db: IDatabase = new MongoDB(new MongoClient(uri).connect());
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+console.log(env);
+console.log(env.NODE_ENV);
+if (env.NODE_ENV === 'development') {
+	if (!global._mongoClientPromise) {
+		client = new MongoClient(uri);
+		global._mongoClientPromise = client.connect();
+	}
+
+	clientPromise = global._mongoClientPromise;
+} else {
+	client = new MongoClient(uri);
+	clientPromise = client.connect();
+}
+
+console.log(client);
+console.log(clientPromise);
+
+export const db: IDatabase = new MongoDB(clientPromise);

e7f0e83e06f70d94e59e919779e46590fb6e70f1
therealvidem <superninario@gmail.com>
2022-05-16 01:46:52 -0700

    Fixed env

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.uri) {
+if (!process.env.MONGODB_URI) {
 	throw new Error('Could not find URI');
 }
 
@@ -14,13 +14,13 @@ let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
 	if (!global._mongoClientPromise) {
-		client = new MongoClient(process.env.uri);
+		client = new MongoClient(process.env.MONGODB_URI);
 		global._mongoClientPromise = client.connect();
 	}
 
 	clientPromise = global._mongoClientPromise;
 } else {
-	client = new MongoClient(process.env.uri);
+	client = new MongoClient(process.env.MONGODB_URI);
 	clientPromise = client.connect();
 }
 
--- src/routes/word/index.json.ts
+++ src/routes/word/index.json.ts
@@ -6,8 +6,8 @@ dotenv.config();
 
 export async function get() {
 	console.log(db);
-	console.log(process.env.uri);
-	const client = new MongoClient(process.env.uri);
+	console.log(process.env.MONGODB_URI);
+	const client = new MongoClient(process.env.MONGODB_URI);
 	const clientPromise = client.connect();
 	console.log(clientPromise);
 	const word = await db.getLatestPlayerWord();
